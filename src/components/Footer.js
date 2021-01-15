59d25cbfc987c1e80ffbc09d872704923ccaea82
frontendben <ericbisby.blagglaw@yahoo.com>
2021-01-15 13:00:11 -0600

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ import { commonStyles } from '../commonStyles';
 const styles = {
   ...commonStyles,
   footer: {
-    width: '90vw',
+    width: '90%',
     minHeight: '10vh',
     display: 'flex',
     justifyContent: 'center',
