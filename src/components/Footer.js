42ca080cb13c38737f9d403eceff10464ab6a277
frontendben <ericbisby.blagglaw@yahoo.com>
2021-01-15 13:13:17 -0600

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -13,7 +13,7 @@ const styles = {
     justifyContent: 'center',
     backgroundColor: '#3E485A',
     padding: '2% 0',
-    margin: '0 4%'
+    margin: '0 4.5%'
   },
   info: {
     display: 'flex',
