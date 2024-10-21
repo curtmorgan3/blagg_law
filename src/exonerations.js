85f2830df2b6b884b4f32d1f89f4f155313598dc
frontendben <ericbisby.blagglaw@yahoo.com>
2024-10-21 13:20:33 -0500

    Update exonerations.js

--- src/exonerations.js
+++ src/exonerations.js
@@ -73,7 +73,7 @@ export const exonerations = [
   {
     name: 'DAVID GECHT',
     img: gecht,
-    bio: "Blagg, working in collaboration with the Exoneration Project, secured the reversal of Gecht's conviction. A judge granted Gecht a new trial after the team presented evidence of the corrupt practices of CPD detective Reynaldo Guevara, who had framed Gecht.",
+    bio: "Blagg, working with the Exoneration Project, secured the reversal of Gecht's conviction. A judge granted Gecht a new trial after the team presented evidence of the corrupt practices of CPD detective Reynaldo Guevara, who had framed Gecht.",
     featured: true,
     link: '/https://chicago.suntimes.com/crime/2022/5/25/23141876/david-gecht-reynaldo-guevara-cpd-detective-police-false-confession-trial-abuse'
   },
